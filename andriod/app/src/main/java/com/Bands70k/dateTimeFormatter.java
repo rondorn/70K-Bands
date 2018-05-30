8ac61eec5001d23028b507554620f9ef4c86a6a1
Ron Dorn <rdorn@salesforce.com>
2018-05-30 08:31:29 -0700

    Made Android changes to support 24 or AmPm. Fixed pointer loader to allow for easier changing of lastYearsDataPointers. Still need to test and possibnly revisit notifications

---
+++ andriod/app/src/main/java/com/Bands70k/dateTimeFormatter.java
@@ -0,0 +1,32 @@
+package com.Bands70k;
+
+import android.text.format.DateFormat;
+import android.util.Log;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * Created by rdorn on 5/29/18.
+ */
+
+public class dateTimeFormatter {
+
+    public static String formatScheduleTime(String timeValue){
+
+        if (DateFormat.is24HourFormat(staticVariables.context) == false){
+            try {
+
+                SimpleDateFormat _24HourSDF = new SimpleDateFormat("HH:mm");
+                SimpleDateFormat _12HourSDF = new SimpleDateFormat("hh:mm a");
+                Date _24HourDt = _24HourSDF.parse(timeValue);
+
+                timeValue = (_12HourSDF.format(_24HourDt));
+            } catch (Exception error) {
+                Log.e("error", "unable to parse time value  " + error.getMessage());
+            }
+        }
+
+        return timeValue;
+    }
+}
