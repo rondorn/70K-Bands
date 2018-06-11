c68509b645b9fa5d9bdec5c61677c847142b803e
Ron Dorn <rdorn@salesforce.com>
2018-06-10 18:07:44 -0700

    Started implementing shows attending tracking

--- swift/70000TonsBands/DetailViewController.swift
+++ swift/70000TonsBands/DetailViewController.swift
@@ -479,4 +479,40 @@ class DetailViewController: UIViewController, UITextViewDelegate{
         }
 
     }
+    
+    @IBAction func clickedOnEvent(_ sender: UITextField) {
+        
+        let fieldText = sender.text;
+        sender.backgroundColor = UIColor.blue
+        let message = "I saw " + bandName + " on " + fieldText!;
+        
+        showToast(message: message);
+        print ("User clicked on an event \(bandName) \(fieldText)");
+
+        //var showsAtten = ShowsAttended();
+        
+        //showsAtten.addShowsAttended();
+        //showsAttendedShowAttendedTracker.addShowsAttended();
+        
+    }
+    
+    func showToast(message : String) {
+        
+        let toastLabel = UILabel(frame: CGRect(x: 10, y: self.view.frame.size.height-250, width: self.view.frame.size.width - 10, height: 45))
+        toastLabel.backgroundColor = UIColor.black.withAlphaComponent(0.6)
+        toastLabel.textColor = UIColor.white
+        toastLabel.textAlignment = .center;
+        toastLabel.font = UIFont(name: "Montserrat-Light", size: 12.0)
+        toastLabel.text = message
+        toastLabel.alpha = 1.0
+        toastLabel.layer.cornerRadius = 10;
+        toastLabel.clipsToBounds  =  true
+        self.view.addSubview(toastLabel)
+        UIView.animate(withDuration: 4.0, delay: 0.1, options: .curveEaseOut, animations: {
+            toastLabel.alpha = 0.0
+        }, completion: {(isCompleted) in
+            toastLabel.removeFromSuperview()
+        })
+    }
+
 }
---
+++ swift/70000TonsBands/ShowsAttended.swift
@@ -0,0 +1,35 @@
+//
+//  ShowsAttended.swift
+//  
+//
+//  Created by Ron Dorn on 6/10/18.
+//
+import Foundation
+
+open class ShowsAttended {
+    
+    //var showsAttendedArray: [String : String] = [String : String]();
+    
+    func addShowsAttended (band: String, location: String, startTime: String, eventType: String)->String{
+        
+        let index = band + ":" + location + ":" + startTime + ":" + eventType
+        
+        var value = ""
+        /*
+        if (showsAttendedArray[index]?.isEmpty == true){
+            value = "Attended";
+            
+        } else if (showsAttendedArray[index] == "Attended"){
+            value = "Partially Attended";
+            
+        } else if (showsAttendedArray[index] == "Attended"){
+            value = "";
+            
+        }
+        */
+        //showsAttendedArray[index] = value
+        
+        return value
+    }
+    
+}
