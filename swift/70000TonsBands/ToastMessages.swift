33bdb40c30fd016dbefa6416f9eaf7f82fe677b4
Ron Dorn <rdorn@salesforce.com>
2018-06-17 09:24:41 -0700

    Fixed toast message is iOS to appear in current scroll view

---
+++ swift/70000TonsBands/ToastMessages.swift
@@ -0,0 +1,93 @@
+//
+//  ToastMessages.swift
+//  70K Bands
+//
+//  Created by Ron Dorn on 6/17/18.
+//  Copyright © 2018 Ron Dorn. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class ToastMessages : UILabel {
+    
+    private let BOTTOM_MARGIN: CGFloat = 16
+    private let SIDE_MARGIN: CGFloat = 16
+    private let HEIGHT: CGFloat = 35
+    private let SHOW_TIME_SECONDS = TimeInterval(3)
+    private let BACKGROUND_COLOR = UIColor.darkGray.withAlphaComponent(0.7).cgColor
+    private let TEXT_COLOR = UIColor.white
+    private let ANIMATION_DURATION_SEC = 0.33
+    
+    private static var queue: [ToastHolder] = []
+    private static var showing: ToastMessages?
+    private static var cellLocationStore: CGRect = CGRect();
+    
+    init(_ text: String) {
+        super.init(frame: CGRect(x: 0, y: 0, width: 0, height: 0))
+        
+        self.text = text
+        self.textColor = TEXT_COLOR
+        textAlignment = .center
+        self.layer.backgroundColor = BACKGROUND_COLOR
+        self.layer.cornerRadius = 5
+    }
+        
+    public func show(_ parent: UIViewController) {
+        
+        show(parent,cellLocation: ToastMessages.cellLocationStore)
+    }
+    
+    public func show(_ parent: UIViewController, cellLocation: CGRect) {
+        
+        //CGRect(layout.Frame.Width / 2 - containerWidth / 2, containerHeight / 2 - 100 / 2, containerWidth, containerHeight);
+        
+        print ("ToastMessage = \(cellLocation) - \(text)")
+        
+        
+        frame = CGRect(x: SIDE_MARGIN, y: cellLocation.midY, width: UIScreen.main.bounds.width - 2 * SIDE_MARGIN, height: HEIGHT)
+        ToastMessages.cellLocationStore = frame
+        
+        if ToastMessages.showing == nil {
+            //Log.d("showing \(String(describing: text))")
+            ToastMessages.showing = self
+            alpha = 0
+            parent.view.addSubview(self)
+            UIView.animate(withDuration: ANIMATION_DURATION_SEC, animations: {
+                self.alpha = 1
+            }, completion: { (completed) in
+                Timer.scheduledTimer(timeInterval: self.SHOW_TIME_SECONDS, target: self, selector: #selector(self.onTimeout), userInfo: nil, repeats: false)
+            })
+        } else {
+            ToastMessages.queue.append(ToastHolder(self, parent))
+        }
+    }
+    
+    @objc func onTimeout() {
+        UIView.animate(withDuration: ANIMATION_DURATION_SEC, animations: {
+            self.alpha = 0
+        }, completion: { (completed) in
+            ToastMessages.showing = nil
+            self.removeFromSuperview()
+            
+            if !ToastMessages.queue.isEmpty {
+                let holder = ToastMessages.queue.removeFirst()
+                holder.toast.show(holder.parent)
+            }
+        })
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("this initializer is not supported")
+    }
+    
+    private class ToastHolder {
+        let toast: ToastMessages
+        let parent: UIViewController
+        
+        init(_ t: ToastMessages, _ p: UIViewController) {
+            toast = t
+            parent = p
+        }
+    }
+}
