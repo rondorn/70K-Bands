585a8c6ca713f69e186722302396288d7ce52115
Ron Dorn <rdorn@salesforce.com>
2018-07-20 07:14:01 -0700

    Fixed offlines bugs in iOS and Android

--- andriod/app/src/main/java/com/Bands70k/OnlineStatus.java
+++ andriod/app/src/main/java/com/Bands70k/OnlineStatus.java
@@ -14,69 +14,22 @@ import android.util.Log;
 
 public class OnlineStatus {
 
-    private static OnlineStatus instance = new OnlineStatus();
+    public static boolean isOnline() {
 
-    static Context context;
+        boolean haveConnectedWifi = false;
+        boolean haveConnectedMobile = false;
 
-    ConnectivityManager connectivityManager;
-    NetworkInfo wifiInfo, mobileInfo;
-    boolean connected = false;
-
-    public static OnlineStatus getInstance(Context ctx) {
-        context = ctx.getApplicationContext();
-        return instance;
-    }
-
-    public boolean isOnline() {
-
-        ConnectivityManager connectivityManager
-                = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
-        NetworkInfo activeNetworkInfo = connectivityManager.getActiveNetworkInfo();
-
-        connected = activeNetworkInfo != null && activeNetworkInfo.isConnected();
-
-        if (connected == true){
-            if (isAirplaneModeOn(context) == true){
-                connected = false;
-            }
+        ConnectivityManager cm = (ConnectivityManager) staticVariables.context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo[] netInfo = cm.getAllNetworkInfo();
+        for (NetworkInfo ni : netInfo) {
+            if (ni.getTypeName().equalsIgnoreCase("WIFI"))
+                if (ni.isConnected())
+                    haveConnectedWifi = true;
+            if (ni.getTypeName().equalsIgnoreCase("MOBILE"))
+                if (ni.isConnected())
+                    haveConnectedMobile = true;
         }
+        return haveConnectedWifi || haveConnectedMobile;
 
-        Log.v("connectivity", "Looks like connected is " + String.valueOf(connected));
-        return connected;
     }
-
-    public static boolean isAirplaneModeOn(Context context) {
-        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.JELLY_BEAN_MR1) {
-            return Settings.System.getInt(context.getContentResolver(),
-                    Settings.System.AIRPLANE_MODE_ON, 0) != 0;
-        } else {
-            return Settings.Global.getInt(context.getContentResolver(),
-                    Settings.Global.AIRPLANE_MODE_ON, 0) != 0;
-        }
-    }
-
-    /*
-    public boolean isOnline() {
-
-        try {
-            connectivityManager = (ConnectivityManager) context
-                    .getSystemService(Context.CONNECTIVITY_SERVICE);
-
-            NetworkInfo networkInfo = connectivityManager.getActiveNetworkInfo();
-            connected = networkInfo != null && networkInfo.isAvailable() &&
-                    networkInfo.isConnected();
-
-            Log.v("connectivity", "Looks like we are connected");
-
-            return connected;
-
-
-        } catch (Exception e) {
-            System.out.println("CheckConnectivity Exception: " + e.getMessage());
-            Log.v("connectivity", e.toString());
-        }
-        return connected;
-    }
-    */
-
 }
