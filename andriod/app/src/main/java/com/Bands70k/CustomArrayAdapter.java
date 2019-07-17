5bb01dbc63b374cffa7113298681cc58d605abfd
Ron Dorn <rdorn@salesforce.com>
2019-07-17 08:34:21 -0700

    Making progress on building new interface for Android. Main layout is done, but is not being properly populated

--- andriod/app/src/main/java/com/Bands70k/CustomArrayAdapter.java
+++ andriod/app/src/main/java/com/Bands70k/CustomArrayAdapter.java
@@ -5,16 +5,25 @@ import android.content.Context;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.graphics.drawable.Drawable;
+import android.text.Layout;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.ListAdapter;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.ImageView;
+
+import com.google.firebase.database.collection.LLRBNode;
 
 import java.util.List;
 
+import static java.lang.Thread.sleep;
+
 public class CustomArrayAdapter extends ArrayAdapter<String> {
+
     protected LayoutInflater inflater;
     protected int layout;
 
@@ -23,6 +32,7 @@ public class CustomArrayAdapter extends ArrayAdapter<String> {
         layout = resourceId;
         inflater = (LayoutInflater)activity.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
 
+
     }
 
     @Override
@@ -30,7 +40,24 @@ public class CustomArrayAdapter extends ArrayAdapter<String> {
 
         View v = inflater.inflate(layout, parent, false);
 
-        v.setBackgroundColor(Color.parseColor("#FF000000"));
+        TextView bandNameCell = (TextView) v.findViewById(R.id.bandNameInCell);
+        TextView locationInCell = (TextView) v.findViewById(R.id.locationInCell);
+        TextView timeInCell = (TextView) v.findViewById(R.id.timeInCell);
+
+        TextView dayLableInCell = (TextView) v.findViewById(R.id.dayLabelInCell);
+        TextView dayInCell = (TextView) v.findViewById(R.id.dayInCell);
+
+        ImageView rankInCell = (ImageView) v.findViewById(R.id.rankingInCell);
+        ImageView eventTypeInCell = (ImageView) v.findViewById(R.id.eventTypeInCell);
+        ImageView attednedInCell = (ImageView) v.findViewById(R.id.attendedInCell);
+
+        bandNameCell.setText("Amon Amarth");
+        locationInCell.setText("Theater Deck 3/4");
+        timeInCell.setText("10:00 pm");
+        dayInCell.setText("1");
+        
+        eventTypeInCell.setImageResource(R.drawable.icon_all_star_jam);
+
         return v;
     }
 }
