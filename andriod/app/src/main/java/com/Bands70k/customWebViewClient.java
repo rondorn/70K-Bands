181a33b81529bd8e16064192083e4a576e664ee1
Ron Dorn <rdorn@salesforce.com>
2023-01-12 12:28:33 -0800

    Added new feature to display better messaging when an empty data is encountered. Is there no data, or is the data filtered.

--- andriod/app/src/main/java/com/Bands70k/customWebViewClient.java
+++ andriod/app/src/main/java/com/Bands70k/customWebViewClient.java
@@ -5,14 +5,19 @@ package com.Bands70k;
  */
 //web view client implementation
 
+import android.util.Log;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
 public class customWebViewClient extends WebViewClient {
-    public boolean shouldOverrideUrlLoading(WebView view, String url)
-    {
+
+    private WebView thisView = null;
+
+    public boolean shouldOverrideUrlLoading(WebView view, String url){
         //do whatever you want with the url that is clicked inside the webview.
         //for example tell the webview to load that url.
+        Log.d("WebView", "Launching web view into " + url);
+        thisView = view;
         view.loadUrl(url);
 
         //return true if this method handled the link event
