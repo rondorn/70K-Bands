c3ffdf51975007e3df64b850e8607dae3c731b2b
Ron Dorn <rdorn@salesforce.com>
2023-09-14 09:59:19 -0700

    Fine tunes alignment issues

--- swift/70000TonsBands/mainListController.swift
+++ swift/70000TonsBands/mainListController.swift
@@ -634,19 +634,23 @@ func getCellValue (_ indexRow: Int, schedule: scheduleHandler, sortBy: String, c
         dayView.text = dayText
         dayLabelView.text = "Day"
         
+        if (indexRow == 0){
+            previousBandName = "Unknown"
+        }
+        print ("Checking if bandname \(bandName) matched previous bandname \(previousBandName) - index for cell \(indexRow)")
         if (bandName == previousBandName && sortBy == "name"){
             
-            var locationString = "   " + location
+            var locationString = "  " + location
             var venueString = NSMutableAttributedString(string: locationString)
             var locationColor = getVenueColor(venue: location)
-            venueString.addAttribute(NSAttributedString.Key.font, value: UIFont.boldSystemFont(ofSize: 20), range: NSRange(location:1,length:1))
-            venueString.addAttribute(NSAttributedString.Key.backgroundColor, value: locationColor, range: NSRange(location:1,length:1))
-            venueString.addAttribute(NSAttributedString.Key.font, value: UIFont.boldSystemFont(ofSize: 18), range: NSRange(location:1,length: location.count))
-            venueString.addAttribute(NSAttributedString.Key.foregroundColor, value: UIColor.lightGray, range: NSRange(location:3,length: location.count))
-            
+            venueString.addAttribute(NSAttributedString.Key.font, value: UIFont.boldSystemFont(ofSize: 19), range: NSRange(location:0,length:1))
+            venueString.addAttribute(NSAttributedString.Key.backgroundColor, value: locationColor, range: NSRange(location:0,length:1))
+            venueString.addAttribute(NSAttributedString.Key.font, value: UIFont.boldSystemFont(ofSize: 19), range: NSRange(location:1,length: location.count))
+            venueString.addAttribute(NSAttributedString.Key.foregroundColor, value: UIColor.lightGray, range: NSRange(location:2,length: location.count))
+
             var locationOfVenue = "  " + (venueLocation[location] ?? "") ?? ""
             var locationOfVenueString = NSMutableAttributedString(string: locationOfVenue)
-            locationOfVenueString.addAttribute(NSAttributedString.Key.font, value: UIFont.boldSystemFont(ofSize: 20), range: NSRange(location:0,length:1))
+            locationOfVenueString.addAttribute(NSAttributedString.Key.font, value: UIFont.boldSystemFont(ofSize: 19), range: NSRange(location:0,length:1))
             locationOfVenueString.addAttribute(NSAttributedString.Key.backgroundColor, value: locationColor, range: NSRange(location:0,length:1))
             locationOfVenueString.addAttribute(NSAttributedString.Key.font, value: UIFont.boldSystemFont(ofSize: 19), range: NSRange(location:1,length: (locationOfVenue.count - 1)))
             locationOfVenueString.addAttribute(NSAttributedString.Key.foregroundColor, value: UIColor.lightGray, range: NSRange(location:2,length: (locationOfVenue.count - 2)))
@@ -663,7 +667,7 @@ func getCellValue (_ indexRow: Int, schedule: scheduleHandler, sortBy: String, c
             var locationString = "  " + locationText
             var myMutableString = NSMutableAttributedString(string: locationString)
             var locationColor = getVenueColor(venue: location)
-            myMutableString.addAttribute(NSAttributedString.Key.font, value: UIFont.boldSystemFont(ofSize: 20), range: NSRange(location: 0,length:1))
+            myMutableString.addAttribute(NSAttributedString.Key.font, value: UIFont.boldSystemFont(ofSize: 19), range: NSRange(location: 0,length:1))
             myMutableString.addAttribute(NSAttributedString.Key.backgroundColor, value: locationColor, range: NSRange(location:0,length:1))
             myMutableString.addAttribute(NSAttributedString.Key.font, value: UIFont.boldSystemFont(ofSize: 19), range: NSRange(location:1,length: locationText.count))
             myMutableString.addAttribute(NSAttributedString.Key.foregroundColor, value: UIColor.lightGray, range: NSRange(location:1,length: locationText.count))
